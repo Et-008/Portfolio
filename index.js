06805a7947efba9c348aec33bc1f5ef13e3ce7d3
Arun et <arunet008@gmail.com>
2021-02-17 19:46:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const port = process.env.port;
+
+app.listen(port, () => {
+    console.log('App has been Started in ', port)
+});
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+});
